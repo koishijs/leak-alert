51e55bf87c1e59b73cc5cb8acdb28e5d9589ff2c
BSTluo <1946831552@qq.com>
2024-02-05 15:08:42 +0800

    新的提示你的koishi已经露出的页面！

--- client/index.ts
+++ client/index.ts
@@ -1,10 +1,17 @@
 import { Context } from '@koishijs/client'
 import 'virtual:uno.css'
 import banner from './banner.vue'
+import exposed from './exposed.vue'
 
 export default (ctx: Context) => {
   ctx.slot({
     type: "global",
     component: banner
+  })
+
+  ctx.page({
+    name: '安全提醒',
+    path: '/exposed',
+    component: exposed
   })
 }
